4f9b6a662a08e71e90e4fc617a0d64c6fe986668
inwsy <inwsy@hotmail.com>
2020-07-27 00:12:27 +0800

    新增「day001」

---
+++ src/com/geekdigging/day001/Solution.java
@@ -0,0 +1,26 @@
+package com.geekdigging.day001;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Date: 2020/7/26
+ * @Time: 23:56
+ * @email: inwsy@hotmail.com
+ * Description:
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Solution solution = new Solution();
+        int[] ints = solution.twoSum(nums, target);
+        System.out.println(ints);
+    }
+}

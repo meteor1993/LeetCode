062cab63fb753df738c9dfbf87febeb1fca251c7
inwsy <inwsy@hotmail.com>
2020-07-27 23:34:22 +0800

    新增 「整数反转」

---
+++ src/com/geekdigging/day002/Solution.java
@@ -0,0 +1,36 @@
+package com.geekdigging.day002;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * 整数反转
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 示例 1:
+ * 输入: 123
+ * 输出: 321
+ * 示例 2:
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ *
+ * @Date: 2020/7/27
+ * @Time: 23:27
+ * @email: inwsy@hotmail.com
+ * Description:
+ */
+public class Solution {
+    public int reverse(int x) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(-1024));
+    }
+}

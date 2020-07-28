495cfecf0dd0898801c5e688865e3d9a2241b55f
inwsy <inwsy@hotmail.com>
2020-07-28 16:51:25 +0800

    更新 「整数反转」 示例代码

--- src/com/geekdigging/day002/Solution.java
+++ src/com/geekdigging/day002/Solution.java
@@ -26,11 +26,29 @@ import java.util.Arrays;
  */
 public class Solution {
     public int reverse(int x) {
-        return 0;
+        int res = 0;
+        while (x != 0) {
+            // 先获取末尾数字
+            int tmp = x % 10;
+
+            if (res < -214748364 || (res == -214748364 && tmp < -8)) {
+                return 0;
+            }
+
+            if (res > 214748364 || (res == 214748364 && tmp > 7)) {
+                return 0;
+            }
+
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+        return res;
     }
+
+    
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.reverse(-1024));
+        System.out.println(solution.reverse(-1463847412));
     }
 }

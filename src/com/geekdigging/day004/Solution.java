1bf80fdaef1033d643c656dcbfebbbd59bd54233
inwsy <inwsy@hotmail.com>
2020-07-30 17:30:40 +0800

    更新 「罗马数字转整数」

--- src/com/geekdigging/day004/Solution.java
+++ src/com/geekdigging/day004/Solution.java
@@ -55,6 +55,19 @@ public class Solution {
         return 0;
     }
 
+    private int getValue(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.romanToInt("LVIII");

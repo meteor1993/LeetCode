0619c1c72ed5d20e1ef038c14dc123385b2e6255
inwsy <inwsy@hotmail.com>
2020-08-02 12:37:06 +0800

    新增 「合并两个有序链表」 示例代码

---
+++ src/com/geekdigging/day007/Solution.java
@@ -0,0 +1,54 @@
+package com.geekdigging.day007;
+
+import com.geekdigging.commonDataStructure.ListNode;
+
+/**
+ * 合并两个有序链表
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ */
+public class Solution {
+
+    // 暴力迭代
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+    // 递归
+    public ListNode mergeTwoLists_1(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+}

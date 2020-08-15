ee48c5c8f6e46b82ed5cdd380d62b2b98ab4c6b6
inwsy <inwsy@hotmail.com>
2020-08-15 20:28:54 +0800

    更新公共数据类型 TreeNode

--- src/com/geekdigging/commonDataStructure/TreeNode.java
+++ src/com/geekdigging/commonDataStructure/TreeNode.java
@@ -9,12 +9,12 @@ package com.geekdigging.commonDataStructure;
  * Description:
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;

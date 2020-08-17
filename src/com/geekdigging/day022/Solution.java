bfbd3a426aeff1545fa96405ed11822edecc5295
inwsy <inwsy@hotmail.com>
2020-08-17 19:22:42 +0800

    新增 「每日一道 LeetCode (22)：二叉树的最大深度」 示例代码

---
+++ src/com/geekdigging/day022/Solution.java
@@ -0,0 +1,60 @@
+package com.geekdigging.day022;
+
+import com.geekdigging.commonDataStructure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 二叉树的最大深度
+ *
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ */
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public int maxDepth_1(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+        int ans = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}

c40521a617a9feb82ff968e0e1ab4f843c1cfb1b
inwsy <inwsy@hotmail.com>
2020-08-25 22:03:58 +0800

    新增 「每日一道 LeetCode (31)： 买卖股票的最佳时机 II」 示例代码

--- src/com/geekdigging/day031/Solution.java
+++ src/com/geekdigging/day031/Solution.java
@@ -47,4 +47,45 @@ public class Solution {
 
         return maxProfit;
     }
+
+    public int maxProfit_1(int[] prices) {
+        return calculate(prices, 0);
+    }
+
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
+
+    public int maxProfit_2(int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+
 }

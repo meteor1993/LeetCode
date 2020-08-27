dfae71de2753aa5823697e260926170a06168da8
inwsy <inwsy@hotmail.com>
2020-08-28 00:15:58 +0800

    新增 「每日一道 LeetCode (33)：只出现一次的数字」 示例代码

---
+++ src/com/geekdigging/day033/Solution.java
@@ -0,0 +1,33 @@
+package com.geekdigging.day033;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.singleNumber(new int[] {4,1,2,1,2}));
+    }
+}

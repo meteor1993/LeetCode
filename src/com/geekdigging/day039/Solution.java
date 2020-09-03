d5764af6dfda201441717419c0ea982f95fcbeb3
inwsy <inwsy@hotmail.com>
2020-09-03 22:45:49 +0800

    更新 「每日一道 LeetCode (39)：多数元素」 示例

--- src/com/geekdigging/day039/Solution.java
+++ src/com/geekdigging/day039/Solution.java
@@ -1,8 +1,6 @@
 package com.geekdigging.day039;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
     public int majorityElement(int[] nums) {
@@ -21,6 +19,34 @@ public class Solution {
         }
 
         return -1;
+    }
+
+    public int majorityElement_1(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
+    public int majorityElement_2(int[] nums) {
+        int count = nums.length / 2;
+        Random rand = new Random();
+        while (true) {
+            int index = rand.nextInt(nums.length);
+            if (countOccurences(nums, nums[index]) > count) {
+                return nums[index];
+            }
+        }
+
+    }
+
+    // 计算 num 出现的次数
+    private int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {

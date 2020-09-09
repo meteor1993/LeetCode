b21955b0803d87a93eccfd581eeddd80921d2ecf
inwsy <inwsy@hotmail.com>
2020-09-09 22:42:54 +0800

    更新 「每日一道 LeetCode (42)：旋转数组」 示例代码

--- src/com/geekdigging/day042/Solution.java
+++ src/com/geekdigging/day042/Solution.java
@@ -57,6 +57,46 @@ public class Solution {
     }
 
     public void rotate_2(int[] nums, int k) {
-        
+        // 先计算 k ，这个 k 为当前需要移动的位置，防止 k 超出数组长度
+        k = k % nums.length;
+        // 初始化移动的次数
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            // 初始化当前位置
+            int current = start;
+            // 初始化要移动的数
+            int prev = nums[start];
+            do {
+                // 计算目标要移动的位置
+                int next = (current + k) % nums.length;
+                // 定义中间变量
+                int tmp = nums[next];
+                // 将要移动的数字进行赋值
+                nums[next] = prev;
+                // 这时，接着移动刚才被挤掉位置的数
+                prev = tmp;
+                // 变化当前的位置为刚才移动的位置
+                current = next;
+                // 将操作次数 +1
+                count++;
+            } while (start != current);
+        }
+    }
+
+    public void rotate_3(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }

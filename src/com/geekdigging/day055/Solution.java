6bafc5a5304f26f186e1bc1f6396f6c75176ea15
ITGXZX-FSG003\weishiyao <inwsy@hotmail.com>
2021-01-14 13:26:17 +0800

    新增 「每日一道 LeetCode (55)：整数转罗马数字」 示例代码

---
+++ src/com/geekdigging/day055/Solution.java
@@ -0,0 +1,21 @@
+package com.geekdigging.day055;
+
+/**
+ * 12. 整数转罗马数字
+ * 贪心算法
+ */
+public class Solution {
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length && num >= 0; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/com/geekdigging/day055/Solution1.java
@@ -0,0 +1,18 @@
+package com.geekdigging.day055;
+
+/**
+ * 12. 整数转罗马数字
+ *
+ * 硬编码方法
+ */
+public class Solution1 {
+    public String intToRoman(int num) {
+
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+    }
+}
